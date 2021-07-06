610c72628f0344a1dc3660b19149412d8dc6e5f7
Bryan Bolivar <bryan.bolivar@koombea.com>
2021-07-06 15:23:34 -0500

    agregado remoto

---
+++ Prueba123/Hola.swift
@@ -0,0 +1,8 @@
+//
+//  Hola.swift
+//  Prueba123
+//
+//  Created by Bryan A Bolivar M on 7/6/21.
+//
+
+import Foundation
---
+++ Prueba123/Prueba123App.swift
@@ -0,0 +1,17 @@
+//
+//  Prueba123App.swift
+//  Prueba123
+//
+//  Created by Bryan A Bolivar M on 7/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Prueba123App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
